b341e8c1c308a3b4df5e81671f3809c442836674
Jimmy Erikson <jimmyerikson@gmail.com>
2017-03-01 12:18:39 +0100

    Update loader.js

--- experiments/CTA-geo/js/loader.js
+++ experiments/CTA-geo/js/loader.js
@@ -19,6 +19,7 @@ frame.onload = function() {
   ['js/mapbox-gl.js', 'js/map.js'].forEach(function(src) {
     var s = doc.createElement('script');
     s.src = src; s.async = false; // defer/async to load framework before map.js
+    s.type="text/javascript";
     doc.head.appendChild(s);
   });
 
